818461223c1cc5a12096a36ba5671100bce346a7
ClocksSugars <clocks.sugars@gmail.com>
2025-11-01 22:34:31 -0700

    moving this to other screen ig

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,57 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import System.IO
+import Data.String
+import Control.Monad.Trans.Except
+
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.String as R
+import System.Directory
+
+import Latexcombinators
+import LaTeXUtils
+import Text.LaTeX.Base.Parser
+import Data.Either.Utils
 
 main :: IO ()
-main = writeFile "ExamplePage.html" $ R.renderHtml pageHTML
+main = do
+   handle <- openFile "latexraw/anatomyRn/philofmath.tex" ReadMode
+   xs <- hGetContents handle
+   writeFile "parsetreeexp3.txt" $ myShow . extractDocument . fromRight . parseLaTeX . fromString $ xs
+   hClose handle
+   --print $ fromRight . parseLaTeX . fromString $ xs
+-- main = outerErr $ do
+--    let filename = "latexraw/anatomyRn/philofmath.tex"
+--    filetext <- obtainFile filename
+--    parseTree <- innerErr (parseLaTeX filetext) InvalidLatex
+--    return ()
+
+innerErr :: (Show a) => Either a b -> (String -> FailureMode) -> Either FailureMode b
+innerErr exs kind = case exs of
+   Left err -> Left . kind . show $ err
+   Right xs -> Right xs
+
+-- outerErr :: ExceptT FailureMode IO () -> IO ()
+-- outerErr eio = case eio of
+--    Right _ -> return ()
+--    Left err -> print err
+
+-- obtainFile :: String -> IO (Either FailureMode String)
+-- obtainFile filename = do
+--    return (if
+--       doesFileExist filename
+--       then Right $
+--          withFile filename ReadMode hGetContents
+--       else Left $ NoSuchFile filename
+--          )
+
+data FailureMode =
+      NoSuchFile String
+   |  InvalidLatex String
+
+--main = writeFile "ExamplePage.html" $ R.renderHtml pageHTML
 
 pageHTML :: Html
 pageHTML = docTypeHtml $ do
